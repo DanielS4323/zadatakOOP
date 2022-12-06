342fad90b86426c6c51a320e3fc9fedc4d763ae1
DanielS4323 <daky4323@gmail.com>
2022-12-06 19:33:17 +0100

    update

--- typescript/zadatak.ts
+++ typescript/zadatak.ts
@@ -1,108 +1,156 @@
 class Heroj {
   ime: string;
   health: number;
-  ranac: Object[];
-  aktivnoOruzje: null;
-  constructor(ime: string, health: number) {
-    this.ime = ime;
-    this.health = health;
-    this.ranac = [];
-    this.aktivnoOruzje = null;
-  }
-
-  //   randomNapad(): number {
-
-  //   }
+  ranac: Magija[] | Oruzje[];
+  aktivnoOruzje: Magija | Oruzje;
 }
 
 class Carobnjak extends Heroj {
-  ime: string;
-  health: number;
-  ranac: Magija[];
-  aktivnoOruzje: any;
-  constructor(ime: string, health: number) {
-    super(ime, health);
-    this.ranac = [];
-    this.aktivnoOruzje = null;
+  private static instance: Carobnjak;
+  private _ime: string;
+  private _health: number;
+  private _ranac: Magija[] = [];
+  private _aktivnoOruzje: Magija;
+  private constructor(ime: string, health: number) {
+    super();
+    this._ime = ime;
+    this._health = health;
   }
-  uzmiOruzje(oruzje: Magija) {
-    if (!(oruzje instanceof Magija)) {
+
+  static instanciraj() {
+    if (Carobnjak.instance) {
+      return this.instance;
+    }
+    this.instance = new Carobnjak("Apolo", 150);
+    return this.instance;
+  }
+
+  uzmiOruzje(oruzje: Magija | Stun) {
+    this._ranac.filter((item) => {
+      if (item === oruzje) {
+        throw Error("Vec imate ovo oruzje!!!");
+      }
+    });
+
+    if (!(oruzje instanceof Magija) && !(oruzje instanceof Stun)) {
       throw Error("Carobnjak moze samo magijom da rukuje");
     }
-    if (this.ranac.length >= 2) {
+
+    if (this._ranac.length >= 2) {
       throw Error("Nije dozvoljeno vise od 2 oruzja!");
     }
-    this.ranac.push(oruzje);
-    console.log(`${this.ime} je pokupio ${oruzje.name}`);
-    this.postaviOruzje(oruzje)
+    this._ranac.push(oruzje);
+    console.log(`${this._ime} je pokupio ${oruzje.name}`);
+    this.postaviOruzje(oruzje);
   }
- private postaviOruzje(oruzje: Magija) {
-    this.aktivnoOruzje = oruzje;
-    console.log(`Oruzje ${oruzje.name} je postavljeno!`)
+  private postaviOruzje(oruzje: Magija) {
+    this._aktivnoOruzje = oruzje;
+    console.log(`Oruzje ${oruzje.name} je postavljeno!`);
   }
 }
 
 class Macevalac extends Heroj {
-  ime: string;
-  health: number;
-  ranac: Oruzje[];
-  aktivnoOruzje: any;
+  private static instance: Macevalac;
+  private _ime: string;
+  private _health: number;
+  private _ranac: Oruzje[] = [];
+  private _aktivnoOruzje: Oruzje;
   constructor(ime: string, health: number) {
-    super(ime, health);
-    this.ranac = [];
-    this.aktivnoOruzje = null;
+    super();
+    this._ime = ime;
+    this._health = health;
   }
 
-  uzmiOruzje(oruzje: Oruzje) {
-    if(!(oruzje instanceof Oruzje)) {
-        throw Error('Macevalac moze samo sa oruzjem da rukuje')
+  static instanciraj() {
+    if (Macevalac.instance) {
+      return this.instance;
     }
-    if(this.ranac.length >= 2) {
-        throw Error('Nije moguce vise od 2 oruzja')
-    }
-    try {
-        this.ranac.push(oruzje)
-        console.log(`${this.ime} je pokupio ${oruzje.name}`)
-        this.postaviOruzje(oruzje)
-    } catch(e) {
-        console.error(e)
-    }
+    this.instance = new Macevalac("Macevalac", 120);
+    return this.instance;
   }
- private postaviOruzje(oruzje: Oruzje) {
-    this.aktivnoOruzje = oruzje
-    console.log(`Oruzje ${oruzje.name} je postavljeno!`)
+
+  uzmiOruzje(oruzje: Mac | Koplje) {
+    this._ranac.filter((item) => {
+      if (item === oruzje) {
+        throw Error("Vec imate ovo oruzje!!!");
+      }
+    });
+
+    if (!(oruzje instanceof Mac) && !(oruzje instanceof Koplje)) {
+      throw Error("Macevalac moze samo sa macem da rukuje");
+    }
+    if (this._ranac.length >= 2) {
+      throw Error("Nije moguce vise od 2 oruzja");
+    }
+
+    this._ranac.push(oruzje);
+    console.log(`${this._ime} je pokupio ${oruzje.name}`);
+    this.postaviOruzje(oruzje);
+  }
+  private postaviOruzje(oruzje: Oruzje) {
+    this._aktivnoOruzje = oruzje;
+    console.log(`Oruzje ${oruzje.name} je postavljeno!`);
   }
 }
 
-class Magija {
+interface Oruzje {
   name: string;
   points: number;
-  constructor(name: string, points: number) {
+}
+
+class Magija implements Oruzje {
+  static instance: Magija = new Magija("carolija", 30);
+  name: string;
+  points: number;
+  private constructor(name: string, points: number) {
     this.name = name;
     this.points = points;
   }
 }
-class Oruzje {
+
+class Mac implements Oruzje {
+  static instance: Mac = new Mac("Mac", 20);
   name: string;
   points: number;
-  constructor(name: string, points: number) {
+  private constructor(name: string, points: number) {
     this.name = name;
     this.points = points;
   }
 }
 
-const carobnjak = new Carobnjak("Apolo", 150);
-const carolija = new Magija("carolija", 20);
-const bljest = new Magija("bljest", 35);
+class Koplje implements Oruzje {
+  static instance: Koplje = new Koplje("Koplje", 25);
+  name: string;
+  points: number;
+  private constructor(name: string, points: number) {
+    this.name = name;
+    this.points = points;
+  }
+}
 
-const macevalac = new Macevalac('macevalac', 100)
-const mac = new Oruzje("mac", 22);
+class Stun implements Oruzje {
+  static instance: Stun = new Stun("Stun", 27);
+  name: string;
+  points: number;
+  private constructor(name: string, points: number) {
+    this.name = name;
+    this.points = points;
+  }
+}
+const koplje = Koplje.instance;
+const mac = Mac.instance;
+const magija = Magija.instance;
+const stun = Stun.instance
 
-macevalac.uzmiOruzje(mac)
+const carobnjak = Carobnjak.instanciraj();
+const macevalac = Macevalac.instanciraj();
 
+// carobnjak.uzmiOruzje(magija)
+// macevalac.uzmiOruzje(koplje)
+// macevalac.uzmiOruzje(mac);
+// macevalac.uzmiOruzje(koplje);
 
+carobnjak.uzmiOruzje(magija)
+carobnjak.uzmiOruzje(stun)
 
-// carobnjak.uzmiOruzje(carolija);
-// carobnjak.uzmiOruzje(bljest);
-
-
+// console.log(carobnjak)
